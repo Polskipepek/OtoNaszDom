1cc42ee875e0311e1dcd6c4bdd139fce9be23ca2
polskipepek <lonely.navel@gmail.com>
2020-02-17 11:49:58 +0100

    Methods: ShowFlats(); GetUsersWithFlats();

--- src/java/com/pepek/businesstier/MainEJB.java
+++ src/java/com/pepek/businesstier/MainEJB.java
@@ -5,22 +5,30 @@
  */
 package com.pepek.businesstier;
 
+import com.pepek.enitities.Flatstable;
 import com.pepek.enitities.Users;
 import com.pepek.integrationTier.FlatstableFacade;
 import com.pepek.integrationTier.UsersFacade;
+import com.pepek.misc.FlatInfo;
 import com.pepek.misc.Utilieties;
+import java.io.File;
+import java.io.IOException;
 import java.io.Serializable;
+import java.nio.file.Files;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
 import java.sql.Date;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Objects;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.ejb.EJB;
 import javax.ejb.Stateless;
-import javax.servlet.http.Part;
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.IOUtils;
 
 /**
  *
@@ -64,8 +72,7 @@ public class MainEJB {
         return false;
     }
 
-    public void AddUserToDB(String user, String password, String email, int telefon, Date date,
-            Utilieties.Sex plec, String adres, Part awatar) throws NoSuchAlgorithmException {
+    public void AddUserToDB(String user, String password, String email, int telefon, Date date, Utilieties.Sex plec, String adres, File awatar) throws NoSuchAlgorithmException {
         String salt = CreateNewSalt();
         MessageDigest digest = MessageDigest.getInstance("SHA-256");
         byte[] encryptedPassword = digest.digest(password.concat(salt).getBytes());
@@ -80,6 +87,71 @@ public class MainEJB {
         random.generateSeed(32);
         System.out.println("randomSalt: " + random);
         return random.toString();
+    }
+
+    public Flatstable AddNewFlatToDB(Users owner, String name, String description, float price, File fileImage) throws IOException {
+        //byte[] temp = Files.readAllBytes(IOUtils.toByteArray(fileImage.toPath()));
+        byte[] temp = convertFileContentToBlob(fileImage.getPath());
+
+        Flatstable mieszkanko = new Flatstable(flatstableFacade.count() + 1, name, description, temp);
+        flatstableFacade.create(mieszkanko);
+
+        return null;
+    }
+
+    public static byte[] convertFileContentToBlob(String filePath) throws IOException {
+        byte[] fileContent = null;
+        try {
+            fileContent = FileUtils.readFileToByteArray(new File(filePath));
+        } catch (IOException e) {
+            throw new IOException("Unable to convert file to byte array. "
+                    + e.getMessage());
+        }
+        return fileContent;
+    }
+
+    public List<Flatstable> ShowFlats(String query, Users owner) {
+        List<Flatstable> flatsRoot = flatstableFacade.findAll();
+        List<Flatstable> flatsInfo = new ArrayList<>();
+
+        if (owner == null) {
+            if (query == null || query.equals("")) {
+                flatsInfo = flatstableFacade.findAll();
+            } else if (query.length() > 0) {
+                for (Flatstable flat : flatsRoot) {
+                    if (flat.getName().contains(query)) {
+                        flatsInfo.add(flat);
+                    }
+                }
+            }
+        } else if (owner != null) {
+
+            if (query.length() > 0) {
+                for (Flatstable flat : flatsRoot) {
+                    if (Objects.equals(owner.getId(), flat.getId()) && (flat.getName().contains(query) || flat.getDescription().contains(query))) {
+                        flatsInfo.add(flat);
+                    }
+                }
+            } else if (query == null || query.length() < 1) {
+                for (Flatstable flat : flatsRoot) {
+                    if (Objects.equals(owner.getId(), flat.getId())) {
+                        flatsInfo.add(flat);
+                    }
+                }
+            }
+        }
+        return flatsInfo;
+    }
+
+    public List<Users> GetUsersWithFlats() {
+        List<Users> usersRoot = usersFacade.findAll();
+        List<Users> users = new ArrayList<>();
+        for (Users user : usersRoot) {
+            if (user.getUserflats().size() > 0) {
+                users.add(user);
+            }
+        }
+        return users;
     }
 
 }
